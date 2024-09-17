e5f74d562dc35a5df6470d52a9180fc38b21f5ec
jycouet <jycouet@gmail.com>
2024-09-17 17:05:26 +0200

    Should be good now

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import Meetup_2023_10_09 from '$lib/meetup/Meetup_2023_10_09.svelte'
 	import Meetup_2024_01_22 from '$lib/meetup/Meetup_2024_01_22.svelte'
+	import Meetup_2024_09_17 from '$lib/meetup/Meetup_2024_09_17.svelte'
 	import Slide from './slide.svelte'
 </script>
 
 <!-- Latest last -->
+<Slide>
+	<Meetup_2024_09_17 />
+</Slide>
 <Slide>
 	<Meetup_2024_01_22 />
 </Slide>
---
+++ src/lib/meetup/Meetup_2024_09_17.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Slide from '../deck/slide.svelte'
+	import Code from '../deck/code.svelte'
+	import Markdown from '../deck/markdown.svelte'
+	import Logo from '$lib/assets/Logo-Dark-Tagline.svg'
+	import Ivan from '$lib/assets/IVAN.jpg'
+	import Svelte5 from '$lib/assets/Svelte5.png'
+</script>
+
+<Slide>
+	<span class="flex">
+		<p>👋 Welcome to</p>
+		<img width="200" alt="logo" src={Logo} />
+	</span>
+	<p>17 / 09 / 2024</p>
+	<p>Starts at 18:30</p>
+</Slide>
+
+<Slide id="list" animate>
+	<p>What's up today?</p>
+	<ul>
+		<li>Intro</li>
+		<br />
+		<li>
+			How to embrace Zod in your SvelteKit applications for schema validation and type safety by
+			Sasan Jaghori
+		</li>
+		<li>why the Svelte 5 API looks the way it does today by Simon Holthausen</li>
+		<li>Should we Date ?</li>
+		<br />
+		<li>Socialize</li>
+	</ul>
+</Slide>
+
+<Slide>
+	<span class="flex">
+		<p>👋 See you next time</p>
+		<img width="200" alt="logo" src={Logo} />
+	</span>
+	<p>10 / 12 / 2024</p>
+	<p style="font-size: small;">https://www.meetup.com/svelte-society-austria/</p>
+</Slide>
+
+<style>
+	.flex {
+		display: flex;
+		gap: 3rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	li {
+		margin-bottom: 2rem;
+		font-size: 2rem;
+	}
+</style>
